0c5ddf998fd2345bfa3ad71c64e49da93ffd57bd
Zeus Notfound <zeusnotfound0@gmail.com>
2025-11-18 18:08:12 +0530

    del: deleted the init sql

--- init.sql
+++
@@ -1,5 +0,0 @@
--- Create database if not exists
-SELECT 'CREATE DATABASE smartclip' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'smartclip');
-
--- Grant all privileges
-GRANT ALL PRIVILEGES ON DATABASE smartclip TO smartclip_user;
